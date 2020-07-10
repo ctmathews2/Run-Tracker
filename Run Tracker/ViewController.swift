8c482ad609ed82e78a7c11b0abd851839e57a9c7
Chandler Mathews <ctmathews2@usfca.edu>
2020-07-10 08:59:36 -0700

    Initial Commit

---
+++ Run Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Run Tracker
+//
+//  Created by Chandler Mathews on 7/10/20.
+//  Copyright © 2020 Chandler Mathews. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
