6f7f893cab668b90dbb32ccb8c2f6dae775e3cba
Chandler Mathews <ctmathews2@usfca.edu>
2020-07-10 09:32:05 -0700

    initial commit

--- Run Tracker/ViewController.swift
+++ Run Tracker/ViewController.swift
@@ -7,14 +7,22 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
 
+    let myDatabase = Database.database().reference()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        /*Auth.auth().signIn(withEmail: "1@2.com", password: "123456") {(user, error) in
+            if error != nil {
+                print(error!)
+            }
+        }*/
+        myDatabase.setValue("Hello World!")
     }
-
-
 }
 
